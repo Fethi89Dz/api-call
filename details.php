a0d41932901e319868dc30d4660163c5ef89d569
Fethi Mohammed Amari <fethi.amari@outlook.com>
2020-10-08 18:50:41 +0100

    Add files via upload
    Details page

---
+++ details.php
@@ -0,0 +1,230 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body  itemtype='https://schema.org/Blog' itemscope='itemscope' class="rtl listing-template-default single single-listing postid-82 weddingcity-nice-select weddingcitychild php-version-7224-0ubuntu018046 weddingcity-desktop no-sidebar weddingcity-1.2.5 swaytheme-rtl elementor-default elementor-kit-2061">
+      <div id="page" class="site">
+         <header itemtype="https://schema.org/WPHeader" itemscope="itemscope" id="masthead">
+            <div class="header header-primary header-fullwidth-transparent fixed-top ">
+               <div class="container-fluid">
+                  <div class="row justify-content-end align-items-center">
+                     <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+                        <div class="header-logo">
+                           <h2 class="weddingcity-site-title"><a href="https://wordpress.happy-season.com/" rel="home">هابي سيزون</a></h2>
+                        </div>
+                     </div>
+                     <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+                        <div class="d-lg-flex justify-content-end align-items-center">
+                           <nav id="navigation" itemtype="https://schema.org/SiteNavigationElement" itemscope="itemscope">
+                           </nav>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
+         <!-- #masthead -->
+         <main id="content" class="site-content">
+            <div id="gallery-1" class="d-none"><a href="">Vendor Gallery 1</a><a href="">Vendor Gallery 2</a></div>
+            <div class="venue-pageheader" style="background: linear-gradient(0deg,rgba(54, 50, 59, 0.35),rgba(43, 38, 47, 0.35)), url(https://wordpress.happy-season.com/wp-content/plugins/weddingcity//assets/images/placeholders/listing/listing-large-img.jpg);">
+               <div class="container">
+                  <div class="row align-items-end page-section">
+                     <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12">
+                        <div class="">
+                           <h1 class="vendor-heading">قاعة أمنيتي للإحتفالات
+                           </h1>
+                           <p class="text-white single-listing-address vendor-address"><span class="mr-2"><i class="fas fa-map-marker-alt "></i></span> جدة </p>
+                        </div>
+                     </div>
+                     <div class="col-xl-5 text-lg-right">
+                        <div class="mt-xl-4">
+                           <a href="#gallery-1" class="btn btn-default btn-gallery">View Gallery</a> <a href="javascript:" data-wishlist="82" class="btn-default-wishlist"><i class="fa fa-heart"></i> 
+                           <span id="single-wishlist-content" class="pl-1">Add To Wishlist</span>
+                           </a> <a href="javascript:void(0);" id="goto_request_quote" class="mt-2 btn btn-default d-lg-none d-xl-none d-md-none d-sm-none">Request Quote</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="vendor-content-wrapper">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-xl-8 col-lg-8 col-md-7 col-sm-12 col-12">
+                        <div class="vendor-meta bg-white border m-0 mb-4">
+                           <div class="vendor-meta-item vendor-meta-item-bordered">
+                              <span class="vendor-price">$550</span>
+                              <span class="vendor-text">Start From</span>
+                           </div>
+                           <div class="vendor-meta-item vendor-meta-item-bordered">
+                              <span class="rating-box vendor-meta-box">
+                              <span class="weddingcity_review rating-star" data-review="4.2"></span>
+                              <span id="1127921950" class="rating-count rating-badge">4.2</span>
+                              <a href="javascript:void(0);" id="goto_review" class="review-comment small"> Write Review</a>
+                              </span>
+                           </div>
+                        </div>
+                        <div class="card border card-shadow-none">
+                           <div class="card-header bg-white">
+                              <h3 class="mb0">About Listing</h3>
+                           </div>
+                           <div class="card-body">
+                              Aenean at urna nec erat placerat porta. Vestibulum et leo mi. Nulla facilisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque laoreet congue rhoncus. Cras efficitur nibh sed velit <b>fringilla, sed sagittis </b>nulla tempus. Nam sit amet turpis in mi facilisis volutpat vel quis lectus.
+                              Maecenas risus urna, feugiat vel elementum vel, rutrum quis ipsum. <b>Quisque tincidunt pellentesque sapien sed luctus.</b> Vivamus venenatis fermentum dui, a convallis turpis venenatis sed. Nullam aliquet, ante in semper facilisis, lectus ante vulputate tortor, sed ullamcorper erat purus id tortor. Integer venenatis consequat mi, convallis sagittis risus vulputate et.
+                              Phasellus sagittis, elit at volutpat laoreet, purus enim tempus enim, id porta eros ipsum eget purus. Aenean aliquet malesuada diam non elementum. Suspendisse eleifend neque ligula, fermentum imperdiet leo pulvinar sed. Nulla eget felis at odio finibus tempor convallis at nisi.
+                              <h3>List of Included in cake</h3>
+                              <ul>
+                                 <li>Quisque non purus bibendum augue sollicitudin hendrerit.</li>
+                                 <li>Sed sed justo vulputate, molestie arcu et, pellentesque erat.</li>
+                                 <li>Cras mollis arcu vehicula semper tincidunt.</li>
+                                 <li>Ut et metus fringilla, ultricies nisl vel, luctus ipsum.</li>
+                                 <li>Suspendisse vehicula lectus at luctus semper.</li>
+                              </ul>
+                              In hac habitasse platea dictumst. Sed dui mi, egestas et mauris vel, mattis tempor dolor. Sed ac ante eu diam faucibus viverra. Cras sem ipsum, dapibus at tortor quis, pretium porta metus. Pellentesque tristique quam a libero mollis iaculis. Donec nec est sit amet sem porta malesuada. Duis maximus sapien ac auctor viverra. Integer imperdiet faucibus blandit.
+                           </div>
+                        </div>
+                        
+                        
+                        <div id="listing_review_process_section" class="card border card-shadow-none leave-review">
+                           <div class="card-header bg-white mb0">
+                              <h3 class="mb0">Write Your Reviews</h3>
+                           </div>
+                           <div class="card-body">
+                              <form method="post" id="listing_review_single_page_section" autocomplete="off">
+                                 <div class="row">
+                                    <div class="col-12">
+                                       <p class="status mb-3"></p>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 col-6"><span class="text-dark">Quality Service</span></div>
+                                    <div class="col-md-9 col-sm-6 col-6">
+                                       <input id="quality_service" name="quality_service" type="hidden" />
+                                       <div class="rateYo"></div>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 col-6"><span class="text-dark">Facilities</span></div>
+                                    <div class="col-md-9 col-sm-6 col-6">
+                                       <input id="facilities" name="facilities" type="hidden" />
+                                       <div class="rateYo"></div>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 col-6"><span class="text-dark">Staff</span></div>
+                                    <div class="col-md-9 col-sm-6 col-6">
+                                       <input id="staff" name="staff" type="hidden" />
+                                       <div class="rateYo"></div>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 col-6"><span class="text-dark">Flexibility</span></div>
+                                    <div class="col-md-9 col-sm-6 col-6">
+                                       <input id="flexibility" name="flexibility" type="hidden" />
+                                       <div class="rateYo"></div>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 col-6"><span class="text-dark">Value of money</span></div>
+                                    <div class="col-md-9 col-sm-6 col-6">
+                                       <input id="value_of_money" name="value_of_money" type="hidden" />
+                                       <div class="rateYo"></div>
+                                    </div>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt30">
+                                       <div class="form-group">
+                                          <label class="control-label" for="user_comment">Write Your Review</label>
+                                          <textarea class="form-control review-comment" rows="5" id="user_comment" name="comment" 
+                                             placeholder="Write Your Review Here." required=""></textarea>
+                                       </div>
+                                    </div>
+                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                       <div class="form-group">
+                                          <label class="control-label" for="user_name">Your Name</label>
+                                          <input id="user_name" name="user_name" type="text" placeholder="Your Name" class="form-control input-md" required="">
+                                       </div>
+                                    </div>
+                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                       <div class="form-group">
+                                          <label class="control-label" for="email">Your Email</label>
+                                          <input id="user_email" name="user_email" type="email" placeholder="Your Email" class="form-control input-md" required="">
+                                       </div>
+                                    </div>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                       <div class="form-group">
+                                          <input type="hidden" id="listing_id" value="82" />
+                                          <button id="submit_listing_review" name="submit_listing_review" class="btn btn-default Submit review" >Submit review</button><input type="hidden" id="venue_review_process" name="venue_review_process" value="d7f217b3fd" /><input type="hidden" name="_wp_http_referer" value="/index.php/listing/creamy-wedding-cake/" />
+                                       </div>
+                                    </div>
+                                 </div>
+                              </form>
+                           </div>
+                        </div>
+                     </div>
+                     <!-- // End of Col-md-8 -->
+                     <div class="col-xl-4 col-lg-4 col-md-5 col-sm-12 col-12">
+                        <div class="sidebar-venue">
+                           <div id="request_quote_form" class="card border card-shadow-none">
+                              <div class="card-body">
+                                 <form id="listing_book_request" method="post" autocomplete="off">
+                                    <div class="row">
+                                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                          <h3 class="mb30">Request Quote</h3>
+                                       </div>
+                                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                          <div class="form-group">
+                                             <input id="request_name" value="" name="request_name" type="text" 
+                                                placeholder="Name" class="form-control input-md" required>
+                                          </div>
+                                       </div>
+                                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                          <div class="form-group">
+                                             <input id="request_email" value="" name="request_email" type="email"
+                                                placeholder="Email" class="form-control input-md" required>
+                                          </div>
+                                       </div>
+                                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                          <div class="form-group">
+                                             <input  id="request_phone" value="" name="request_phone" type="number" 
+                                                placeholder="Phone" class="form-control input-md" required>
+                                          </div>
+                                       </div>
+                                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                          <div class="form-group">
+                                             <input id="weddingdate" value="" name="weddingdate" type="text" 
+                                                placeholder="Wedding Date" 
+                                                class="form-control input-md wedding_date" reqired>
+                                             <div class="venue-form-calendar"><i class="far fa-calendar-alt"></i></div>
+                                          </div>
+                                       </div>
+                                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                          <div class="form-group">
+                                             <textarea class="form-control" id="request_comment"
+                                                name="request_comment" rows="6" 
+                                                placeholder="Write Comment" required></textarea>
+                                          </div>
+                                       </div>
+                                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                          <div class="form-group">
+                                             <button type="submit" class="btn btn-default btn-block">Submit Quote</button>
+                                             <input type="hidden" id="listing_post_id" value="82" >
+                                             <input type="hidden" id="listing_author_id" value="0" >
+                                             <input type="hidden" id="request_for_listing" name="request_for_listing" value="6a46c42b39" /><input type="hidden" name="_wp_http_referer" value="/index.php/listing/creamy-wedding-cake/" />
+                                          </div>
+                                       </div>
+                                    </div>
+                                 </form>
+                              </div>
+                           </div>
+                           <div class="widget claim-widget">
+                              <div class="claim-widget-content">
+                                 <h4 class="widget-title">Is this your business ?</h4>
+                                 <p>Claim listing is the best way to manage and protect your business.</p>
+                                 <a href="https://wordpress.happy-season.com/index.php/vendor-login-registration/" class="btn btn-primary">Claim Now</a>                    
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <!-- //. col-md-4 Ending... -->
+                  </div>
+                  <!-- Row Ending -->
+               </div>
+            </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </main><!-- #content area end -->
+
+   </body>
+</html>
